eea4dfa8884b07675a4f32db6e35fab76b44f216
saheb <sahibbhalla.010@gmail.com>
2022-08-07 15:45:05 -0400

    Added Api Calls, render current weather and next day five

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,505 @@
+var data = {
+  lat: 43.6532,
+  lon: -79.3832,
+  timezone: "America/Toronto",
+  timezone_offset: -14400,
+  current: {},
+  minutely: [],
+  hourly: [],
+  daily: [
+    {
+      dt: 1659891600,
+      sunrise: 1659867192,
+      sunset: 1659918806,
+      moonrise: 1659906120,
+      moonset: 1659848280,
+      moon_phase: 0.33,
+      temp: {
+        day: 303.97,
+        min: 296.51,
+        max: 304.08,
+        night: 297.32,
+        eve: 297.71,
+        morn: 296.6,
+      },
+      feels_like: {
+        day: 308.88,
+        night: 298.14,
+        eve: 298.57,
+        morn: 297.22,
+      },
+      pressure: 1015,
+      humidity: 65,
+      dew_point: 296.63,
+      wind_speed: 4.59,
+      wind_deg: 191,
+      wind_gust: 9.29,
+      weather: [
+        {
+          id: 501,
+          main: "Rain",
+          description: "moderate rain",
+          icon: "10d",
+        },
+      ],
+      clouds: 88,
+      pop: 0.74,
+      rain: 6.05,
+      uvi: 6.74,
+    },
+    {
+      dt: 1659978000,
+      sunrise: 1659953659,
+      sunset: 1660005125,
+      moonrise: 1659996900,
+      moonset: 1659937440,
+      moon_phase: 0.37,
+      temp: {
+        day: 299.26,
+        min: 294.81,
+        max: 300.31,
+        night: 294.81,
+        eve: 299.54,
+        morn: 296.26,
+      },
+      feels_like: {
+        day: 299.26,
+        night: 295.43,
+        eve: 299.54,
+        morn: 297.05,
+      },
+      pressure: 1012,
+      humidity: 78,
+      dew_point: 294.94,
+      wind_speed: 7.5,
+      wind_deg: 234,
+      wind_gust: 12.13,
+      weather: [
+        {
+          id: 501,
+          main: "Rain",
+          description: "moderate rain",
+          icon: "10d",
+        },
+      ],
+      clouds: 91,
+      pop: 0.99,
+      rain: 5.15,
+      uvi: 3.85,
+    },
+    {
+      dt: 1660064400,
+      sunrise: 1660040126,
+      sunset: 1660091443,
+      moonrise: 1660087140,
+      moonset: 1660027380,
+      moon_phase: 0.41,
+      temp: {
+        day: 295.54,
+        min: 290.31,
+        max: 297.4,
+        night: 293.39,
+        eve: 296.57,
+        morn: 290.31,
+      },
+      feels_like: {
+        day: 295.24,
+        night: 293.22,
+        eve: 296.38,
+        morn: 290.04,
+      },
+      pressure: 1018,
+      humidity: 54,
+      dew_point: 285.51,
+      wind_speed: 6.32,
+      wind_deg: 324,
+      wind_gust: 10.12,
+      weather: [
+        {
+          id: 803,
+          main: "Clouds",
+          description: "broken clouds",
+          icon: "04d",
+        },
+      ],
+      clouds: 65,
+      pop: 0.79,
+      uvi: 7.66,
+    },
+    {
+      dt: 1660150800,
+      sunrise: 1660126594,
+      sunset: 1660177760,
+      moonrise: 1660176540,
+      moonset: 1660118100,
+      moon_phase: 0.45,
+      temp: {
+        day: 297.89,
+        min: 290.97,
+        max: 298.98,
+        night: 295.55,
+        eve: 297.16,
+        morn: 291.75,
+      },
+      feels_like: {
+        day: 297.86,
+        night: 295.6,
+        eve: 297.24,
+        morn: 291.49,
+      },
+      pressure: 1017,
+      humidity: 55,
+      dew_point: 287.53,
+      wind_speed: 3.61,
+      wind_deg: 149,
+      wind_gust: 3.56,
+      weather: [
+        {
+          id: 802,
+          main: "Clouds",
+          description: "scattered clouds",
+          icon: "03d",
+        },
+      ],
+      clouds: 46,
+      pop: 0,
+      uvi: 7.89,
+    },
+    {
+      dt: 1660237200,
+      sunrise: 1660213061,
+      sunset: 1660264075,
+      moonrise: 1660265340,
+      moonset: 1660209300,
+      moon_phase: 0.5,
+      temp: {
+        day: 298.22,
+        min: 293.37,
+        max: 298.22,
+        night: 293.52,
+        eve: 295.02,
+        morn: 294.25,
+      },
+      feels_like: {
+        day: 298.17,
+        night: 292.79,
+        eve: 294.62,
+        morn: 294.09,
+      },
+      pressure: 1012,
+      humidity: 53,
+      dew_point: 287.62,
+      wind_speed: 7.13,
+      wind_deg: 333,
+      wind_gust: 10.91,
+      weather: [
+        {
+          id: 500,
+          main: "Rain",
+          description: "light rain",
+          icon: "10d",
+        },
+      ],
+      clouds: 62,
+      pop: 0.45,
+      rain: 0.73,
+      uvi: 0.48,
+    },
+    {
+      dt: 1660323600,
+      sunrise: 1660299529,
+      sunset: 1660350389,
+      moonrise: 1660353660,
+      moonset: 1660300620,
+      moon_phase: 0.52,
+      temp: {
+        day: 296.03,
+        min: 288.64,
+        max: 297.12,
+        night: 291.93,
+        eve: 294.43,
+        morn: 288.64,
+      },
+      feels_like: {
+        day: 295.29,
+        night: 291.38,
+        eve: 293.92,
+        morn: 288.07,
+      },
+      pressure: 1019,
+      humidity: 35,
+      dew_point: 279.57,
+      wind_speed: 5.51,
+      wind_deg: 357,
+      wind_gust: 9.77,
+      weather: [
+        {
+          id: 800,
+          main: "Clear",
+          description: "clear sky",
+          icon: "01d",
+        },
+      ],
+      clouds: 1,
+      pop: 0,
+      uvi: 1,
+    },
+    {
+      dt: 1660410000,
+      sunrise: 1660385997,
+      sunset: 1660436702,
+      moonrise: 1660441560,
+      moonset: 1660391880,
+      moon_phase: 0.56,
+      temp: {
+        day: 296.5,
+        min: 290.25,
+        max: 297.12,
+        night: 293.65,
+        eve: 295.57,
+        morn: 290.48,
+      },
+      feels_like: {
+        day: 295.99,
+        night: 293.09,
+        eve: 295.07,
+        morn: 289.78,
+      },
+      pressure: 1018,
+      humidity: 42,
+      dew_point: 282.07,
+      wind_speed: 2.61,
+      wind_deg: 128,
+      wind_gust: 3.98,
+      weather: [
+        {
+          id: 800,
+          main: "Clear",
+          description: "clear sky",
+          icon: "01d",
+        },
+      ],
+      clouds: 2,
+      pop: 0,
+      uvi: 1,
+    },
+    {
+      dt: 1660496400,
+      sunrise: 1660472465,
+      sunset: 1660523013,
+      moonrise: 1660529340,
+      moonset: 1660482900,
+      moon_phase: 0.6,
+      temp: {
+        day: 298.74,
+        min: 290.98,
+        max: 299.65,
+        night: 295.21,
+        eve: 297.22,
+        morn: 291.96,
+      },
+      feels_like: {
+        day: 298.32,
+        night: 294.93,
+        eve: 296.91,
+        morn: 291.49,
+      },
+      pressure: 1014,
+      humidity: 37,
+      dew_point: 282.34,
+      wind_speed: 2.63,
+      wind_deg: 352,
+      wind_gust: 4.56,
+      weather: [
+        {
+          id: 803,
+          main: "Clouds",
+          description: "broken clouds",
+          icon: "04d",
+        },
+      ],
+      clouds: 73,
+      pop: 0,
+      uvi: 1,
+    },
+  ],
+  alerts: [
+    {
+      sender_name: "Environment Canada",
+      event: "heat",
+      start: 1659883516,
+      end: 1659941116,
+      description:
+        "\nHeat event is expected to continue today. \n\nMaximum temperatures are expected to be in the low thirties with minimum temperatures in the low to mid-twenties, bringing little relief from the heat. \n\nHumidex values are expected to reach the low forties. \n\nSlightly cooler temperatures are expected on Monday. \n\nHot and humid air can also bring deteriorating air quality and can result in the Air Quality Health Index approaching the high risk category.\n\n###\n\nWatch for the effects of heat illness: swelling, rash, cramps, fainting, heat exhaustion, heat stroke and the worsening of some health conditions.\n\nHeat warnings are issued when very high temperature or humidity conditions are expected to pose an elevated risk of heat illnesses, such as heat stroke or heat exhaustion.\n\nPlease continue to monitor alerts and forecasts issued by Environment Canada. To report severe weather, send an email to ONstorm@ec.gc.ca or tweet reports using #ONStorm.\n",
+      tags: ["Extreme temperature value"],
+    },
+  ],
+};
+var searchButton = $("#search"); //Search button selector
+var tempCityHolder = "";
+//Set color for UV index List depending on Uv Index
+var setHumidColor = function (data) {
+  $(".mainList").css("list-style", "none");
+  if (Number(data) < 3) {
+    $(".indexValue").css("background-color", "green");
+  } else if (Number(data) < 5) {
+    $(".indexValue").css("background-color", "orange");
+  } else if (Number(data) < 7) {
+    $(".indexValue").css("background-color", "red");
+  } else if (Number(data) < 10) {
+    $(".indexValue").css("background-color", "yellow");
+  } else {
+    $(".indexValue").css("background-color", "purple");
+  }
+};
+var createMainDiv = function (data) {
+  //Create MainDiv
+  var date = new Date(data.current.dt * 1000); //Convert Unix date
+  var city = $("<h2>") //set city and Date
+    .text(
+      tempCityHolder +
+        " (" +
+        date.getMonth() +
+        "/" +
+        date.getDate() +
+        "/" +
+        date.getFullYear() +
+        ")"
+    )
+    .addClass("cityName")
+    .css("display", "inline-block");
+  var image = $("<img>") //set Image Icon
+    .attr(
+      "src",
+      "http://openweathermap.org/img/wn/" +
+        data.current.weather[0].icon +
+        ".png"
+    )
+    .addClass("iconImage")
+    .css("display", "inline-block");
+  var list = $("<ul>").addClass("mainList"); //Ul set calss
 
-var createMainDiv = function(data){
-  var date = new Date(data.dt * 1000)
-  var city =$("<h2>").text(data.name+"("+date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear()+")").addClass("cityName").css('display', 'inline-block');
-  var image = $("<img>").attr("src", "http://openweathermap.org/img/wn/"+data.weather[0].icon+".png").addClass("iconImage").css('display', 'inline-block');
-  var list =$("<ul>").addClass("mainList")
-  list.append($("<li>").text("Temp "+data.temp))
-
-  $("#mainDisplay").append(city,image,list);
-}
-
-var displayData = function (data) {
-    createMainDiv(data)
+  list.append(
+    //temp item
+    $("<li>").text(
+      "Temp: " + Number(data.current.temp - 273.15).toFixed(1) + " C"
+    )
+  );
+  list.append(
+    //Wind Item
+    $("<li>").text("Wind: " + Number(data.current.wind_speed) + " MPH")
+  );
+  list.append(
+    //Humidity Item
+    $("<li>").text("Humidity: " + Number(data.current.humidity) + "%")
+  );
+  list.append(
+    //UvIndex Item
+    $("<li>")
+      .attr("id", "uvIndex")
+      .text("UV Index ")
+      .append(
+        $("<button>")
+          .addClass("indexValue btn")
+          .text(Number(data.current.uvi).toFixed(1))
+      )
+  );
+  $("#mainDisplay").append(city, image, list); //Append every element to UL
+  setHumidColor(Number(data.current.uvi)); //Set Color
 };
 
-var getWeather = function () {
-  var url =
-    "https://api.openweathermap.org/data/2.5/weather?q=shimla&appid=1570abab448b8f7eb6d7d0a44bd3cbef";
-    // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=1570abab448b8f7eb6d7d0a44bd3cbef"
-  // var Url="https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=85a8d1f30e19c5a51796de9c2748d523"
-  fetch(url).then(function (response) {
+var createDayForcast = function (
+  data //day Forcast for next 5 days
+) {
+  var secondary = $("#secondaryDisplay");
+  secondary.append($("<h1>").text("5-Day Forecast:"));
+  for (var i = 0; i < data.daily.length; i++) {
+    var date = new Date(data.daily[i].dt * 1000);
+    var list = $("<ul>").addClass("dailyList");
+    list.append(
+      //temp item
+      $("<li>").text(
+        "Temp: " + Number(data.daily[i].temp.day - 273.15).toFixed(1) + " C"
+      )
+    );
+    list.append(
+      //Wind Item
+      $("<li>").text("Wind: " + Number(data.daily[i].wind_speed) + " MPH")
+    );
+    list.append(
+      //Humidity Item
+      $("<li>").text("Humidity: " + Number(data.daily[i].humidity) + "%")
+    );
+    secondary.append(
+      $("<div>")
+        .addClass("card dayWeather col-sm-2")
+        .append(
+          $("<h4>").text(
+              " (" +
+              date.getMonth() +
+              "/" +
+              date.getDate() +
+              "/" +
+              date.getFullYear() +
+              ")"
+          ),
+          $("<img>").attr(
+            "src",
+            "http://openweathermap.org/img/wn/" +
+              data.daily[i].weather[0].icon +
+              ".png"
+          ).addClass("IconImage"),list
+          
+        )
+    );
+  }
+};
+var displayData = function (
+  data //Render method
+) {
+  createMainDiv(data); //Display current weather
+  createDayForcast(data); //displayNext  5 day weather
+};
+var getGeoCode = function (address) {
+  //Set LAt and long from google API
+  var Url =
+    "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+    address +
+    "&key=AIzaSyCXj93E80QFMSeJNrWWSLyCBn8BdyL9JGY";
+
+  fetch(Url).then(function (response) {
     return response.json().then(function (data) {
-      displayData(data);
+      getWeather(
+        //Open weather API
+        data.results[0].geometry.location.lat, //lat
+        data.results[0].geometry.location.lng //long
+      );
     });
   });
 };
-getWeather();
+var searchAction = function () {
+  //search action method
+  if ($("#cityInput").val() !== "") {
+    tempCityHolder = $("#cityInput").val();
+    getGeoCode($("#cityInput").val()); //get Geo code from Google API
+    $("#cityInput").val(" "); //set the input to empty string
+  } else {
+    alert("Please enter a valid address");
+  }
+};
+var getWeather = function (lat, long) {
+  //OpenWeather API
+  var url =
+    "https://api.openweathermap.org/data/3.0/onecall?lat=" +
+    lat +
+    "&lon=" +
+    long +
+    "&appid=7db3669243ac203e0995a90c5dfd0558";
+  fetch(url).then(function (response) {
+    return response.json().then(function (data) {
+      displayData(data); //After all the API call, initiate this method
+    });
+  });
+};
+searchButton.on("click", searchAction);//Search button action
+// createDayForcast(data);
